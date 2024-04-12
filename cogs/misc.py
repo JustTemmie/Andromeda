c24715eb5aea7d7e113373bafe9b4edc457a9294
JustTemmie <47639983+JustTemmie@users.noreply.github.com>
2024-04-12 18:17:04 +0200

    boom

--- cogs/misc.py
+++ cogs/misc.py
@@ -3,13 +3,16 @@ from discord.ext import commands
 
 import hatsune_miku.helpers as helpers
 
+import hatsune_miku.APIs.tenor as tenorLib
+
+
 class Misc(commands.Cog):
     def __init__(self, miku):
         self.miku = miku
 
     @commands.command(name="explode")
     async def explodeCommand(self, ctx):
-        await ctx.send("boom")
+        await ctx.send("https://tenor.com/view/miku-hatsune-hatsune-miku-miku-explosion-explosion-gif-gif-26996455")
 
 
 async def setup(miku):
