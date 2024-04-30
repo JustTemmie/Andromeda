e3413aeee11e5e76728bd66090ab673bebbd1dc2
JustTemmie <47639983+JustTemmie@users.noreply.github.com>
2024-04-30 13:14:15 +0200

    default the embed colours to white

--- hatsune_miku/helpers.py
+++ hatsune_miku/helpers.py
@@ -18,23 +18,27 @@ def getProgressBar(current, max, width = 20):
 # user still needs to set the title, description, and fields
 def create_embed(user):
     embed = discord.Embed()
-    embed.colour = user.colour
+    # set the colour to the user colour, unless it's black, in that case default to white (#ffffff)
+    embed.colour = user.colour if user.colour != discord.Colour(000000) else discord.Colour(16777215)
     embed.set_footer(text=f"invoked by {user.display_name}", icon_url=user.display_avatar.url)
     
     return embed
 
 def format_time(seconds):
     time_table = {
-        "days": seconds // 86400,
-        "hours": (seconds // 3600) % 86400,
-        "minutes": (seconds // 60) % 60,
-        "seconds": seconds % 60,
+        "year": (seconds // 31557600) % 31557600,
+        "day": (seconds // 86400) % 86400,
+        "hour": (seconds // 3600) % 3600,
+        "minute": (seconds // 60) % 60,
+        "second": seconds % 60,
     }
     
     return_string = ""
     
     for unit, value in time_table.items():
-        if value > 0:
+        if value > 1:
+            return_string += f"{value} {unit}s "
+        elif value == 1:
             return_string += f"{value} {unit} "
     
     return return_string
