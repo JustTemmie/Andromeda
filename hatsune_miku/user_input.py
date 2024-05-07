de1a41b770131b077fb7ece1341ec6de3ffc5191
JustTemmie <47639983+JustTemmie@users.noreply.github.com>
2024-05-07 16:14:04 +0200

    library update

---
+++ hatsune_miku/user_input.py
@@ -0,0 +1,19 @@
+import asyncio
+
+async def get_input(miku, ctx, time=20, bonus=""):
+    try:
+        response = await miku.wait_for("message", check=lambda m: m.author == ctx.author, timeout=time)
+        return response
+    except asyncio.TimeoutError:
+        await ctx.send(f"**Timed out** You took too long to answer the question{bonus}")
+        return None
+
+async def get_consent(miku, ctx, time, bonus=""):
+    response = await get_input(miku, ctx, time, bonus)
+    if response == None:
+        return False
+    
+    if response.content.lower() in ["yes", "yes.", "ye", "ye.", "y", "y."]:
+        return True
+    else:
+        return False
