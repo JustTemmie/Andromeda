adb7c4f3bc5dd304d0c1cf56fd073ee44270c211
JustTemmie <47639983+JustTemmie@users.noreply.github.com>
2024-05-18 18:06:41 +0200

    aaaa

--- cogs/events/after_invoke.py
+++ cogs/events/after_invoke.py
@@ -8,13 +8,13 @@ class AfterInvoke(commands.Cog):
     def __init__(self, miku):
         self.miku = miku
 
-    # @commands.Cog.listener()
-    # async def on_command(self, ctx):
-    #     pass
+    @commands.Cog.listener()
+    async def on_command(self, ctx):
+        pass
     
-    # @commands.Cog.listener()
-    # async def on_command_completion(self, ctx):
-    #     pass
+    @commands.Cog.listener()
+    async def on_command_completion(self, ctx):
+        pass
     
 async def setup(miku):
     await miku.add_cog(AfterInvoke(miku))
--- main.py
+++ main.py
@@ -14,111 +14,99 @@ import os
 
 import hatsune_miku.APIs.config as configLib 
 
-miku = None
 
-if __name__ == "__main__":
-    config = configLib.getConfig()
-    settings = configLib.getSettings()
-    
-    directories_to_make = ["local_only"]
-    directories_to_empty = ["temp", "logs"]
-    
-    for dir in directories_to_make + directories_to_empty:
-        if not os.path.exists(dir):
-            os.mkdir(dir)
-            
-    if config["DEVELOPMENT"]:
-        for dir in directories_to_empty:
-            for file in os.listdir(dir):
-                os.remove(f"{dir}/{file}")
-    
-    logger = logging.getLogger('discord')
-    logger.setLevel(logging.INFO)
-    logging.getLogger('discord.http').setLevel(logging.INFO)
+config = configLib.getConfig()
+settings = configLib.getSettings()
 
-    handler = logging.handlers.RotatingFileHandler(
-        filename='logs/discord.log',
-        encoding='utf-8',
-        maxBytes=32 * 1024 * 1024,  # 32 MiB
-        backupCount=5,  # Rotate through 5 files
-    )
-    dt_fmt = '%Y-%m-%d %H:%M:%S'
-    formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+directories_to_make = ["local_only"]
+directories_to_empty = ["temp", "logs"]
 
-
-    class Miku(commands.AutoShardedBot):
-        def __init__(self, *args, **kwargs):
-            self.config = config
-            self.settings = settings
-            
-            self.ready = False
-            
-            super().__init__(
-                shards=self.config["SHARDS"],
-                command_prefix=(self.get_prefix),
-                strip_after_prefix=True,
-                case_insensitive=True,
-                owner_ids=self.config["OWNER_IDS"],
-                intents=discord.Intents.all(),
-            *args, **kwargs)
-
-            self.start_time = datetime.now()
-
-        async def get_prefix(self, message):
-            return commands.when_mentioned_or(*self.config["PREFIXES"])(self, message)
-
-        async def on_ready(self) -> None:
-            print(f"Succesfully logged in as {self.user}")
-            self.ready = True
-
-        async def setup_hook(self) -> None:
-            async def sync_tree(self):
-                print(f"Syncing command tree...")
-                if self.config["DEVELOPMENT"]:
-                    guild = discord.Object(id=config["DEVELOPMENT_GUILD"])
-                    self.tree.copy_global_to(guild=guild)
-                    await self.tree.sync()
-                else:
-                    await self.tree.sync()
-                print(f"Command tree synced!")
-                
-            if self.config["SYNC_TREE"]:
-                await sync_tree(self)
-            else:
-                print("miku is set to not sync tree, continuing")
+for dir in directories_to_make + directories_to_empty:
+    if not os.path.exists(dir):
+        os.mkdir(dir)
         
+if config["DEVELOPMENT"]:
+    for dir in directories_to_empty:
+        for file in os.listdir(dir):
+            os.remove(f"{dir}/{file}")
 
-    miku = Miku()
-    # miku.tree = discord.app_commands.CommandTree(miku)
-    # miku.remove_command("help")
+logger = logging.getLogger('discord')
+logger.setLevel(logging.INFO)
+logging.getLogger('discord.http').setLevel(logging.INFO)
 
-    @miku.tree.command(
-        name="commandname",
-        description="My first application Command",
-        guild=discord.Object(id=885113462378876948)
-    )
-    async def first_command(interaction):
-        await interaction.response.send_message("Hello!")
-    
-    
-    async def main():
-        async with miku:
-            if len(miku.config["COG_LIST_OVERWRITE"]) >= 1:
-                for cog in miku.config["COG_LIST_OVERWRITE"]:
-                    await miku.load_extension(f"cogs.{cog}")
-            
+handler = logging.handlers.RotatingFileHandler(
+    filename='logs/discord.log',
+    encoding='utf-8',
+    maxBytes=32 * 1024 * 1024,  # 32 MiB
+)
+dt_fmt = '%Y-%m-%d %H:%M:%S'
+formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
+class Miku(commands.AutoShardedBot):
+    def __init__(self, *args, **kwargs):
+        self.config = config
+        self.settings = settings
+        
+        self.ready = False
+        
+        super().__init__(
+            shards=self.config["SHARDS"],
+            command_prefix=(self.get_prefix),
+            strip_after_prefix=True,
+            case_insensitive=True,
+            owner_ids=self.config["OWNER_IDS"],
+            intents=discord.Intents.all(),
+        *args, **kwargs)
+
+        self.start_time = datetime.now()
+
+    async def get_prefix(self, message):
+        return commands.when_mentioned_or(*self.config["PREFIXES"])(self, message)
+
+    async def on_ready(self) -> None:
+        print(f"Succesfully logged in as {self.user}")
+        self.ready = True
+
+    async def setup_hook(self) -> None:
+        async def sync_tree(self):
+            print(f"Syncing command tree...")
+            if self.config["DEVELOPMENT"]:
+                guild = discord.Object(id=config["DEVELOPMENT_GUILD"])
+                self.tree.copy_global_to(guild=guild)
+                await self.tree.sync()
             else:
-                for filename in glob.iglob("./cogs/**", recursive=True):
-                    if filename.endswith(".py"):
-                        # goes from "./cogs/economy.py" to "cogs.economy"
-                        filename = filename[2:].replace("/", ".")[:-3]
-                        await miku.load_extension(filename)
+                await self.tree.sync()
+            print(f"Command tree synced!")
             
-            await miku.start(miku.config["API_KEYS"]["DISCORD"])
+        if self.config["SYNC_TREE"]:
+            await sync_tree(self)
+        else:
+            print("miku is set to not sync tree, continuing")
+    
 
+# miku.tree = discord.app_commands.CommandTree(miku)
+# miku.remove_command("help")
 
-    miku.loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(miku.loop)
-    asyncio.run(main())
+async def main(miku):
+    async with miku:
+        if len(miku.config["COG_LIST_OVERWRITE"]) >= 1:
+            for cog in miku.config["COG_LIST_OVERWRITE"]:
+                await miku.load_extension(f"cogs.{cog}")
+        
+        else:
+            for filename in glob.iglob("./cogs/**", recursive=True):
+                if filename.endswith(".py"):
+                    # goes from "./cogs/economy.py" to "cogs.economy"
+                    filename = filename[2:].replace("/", ".")[:-3]
+                    await miku.load_extension(filename)
+        
+        await miku.start(miku.config["API_KEYS"]["DISCORD"])
+
+miku = Miku()
+
+miku.loop = asyncio.new_event_loop()
+asyncio.set_event_loop(miku.loop)
+asyncio.run(main())
