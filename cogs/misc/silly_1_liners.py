597448ca761c5671aa4bbb2bb4231bf2c8d5cc9f
JustTemmie <47639983+JustTemmie@users.noreply.github.com>
2024-07-28 11:03:21 +0200

    i should really test these before pushing

--- cogs/misc/silly_1_liners.py
+++ cogs/misc/silly_1_liners.py
@@ -23,7 +23,7 @@ class Silly(commands.Cog):
     
     @app_commands.command(
         name="dance",
-        description=="command_brief_dance",
+        description=="command_brief_dance"
     )
     @app_commands.allowed_installs(guilds=True, users=True)
     @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
