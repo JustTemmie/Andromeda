2cee95e3779174ef22a996c02635bccefa687e18
JustTemmie <47639983+JustTemmie@users.noreply.github.com>
2025-02-19 16:00:13 +0100

    does this work?

--- cogs/events/voice_channel_monitor.py
+++ cogs/events/voice_channel_monitor.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+import logging
+
 class VoiceChannelMonitor(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -8,15 +10,18 @@ class VoiceChannelMonitor(commands.Cog):
     
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
+        logging.info(f"{member.id} joined vc {after.channel.id}")
         if member.id == self.user_to_monitor and after.channel is not None:
             for channel_member in after.channel.members:
                 if channel_member.id == self.target_user:
+                    logging.info("kicking usr")
                     await member.move_to(None)
                     return
         
         if member.id == self.target_user and after.channel is not None:
             for channel_member in after.channel.members:
                 if channel_member.id == self.user_to_monitor:
+                    logging.info("kicking usr2")
                     await after.channel.guild.get_member(self.user_to_monitor).move_to(None)
                     return
     
